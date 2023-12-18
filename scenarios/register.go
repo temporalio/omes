98ad5ed84a523915d0cfe6827301ed55e642f70c
Dan Davison <dan.davison@temporal.io>
2023-12-18 16:55:14 -0500

    Add exposed RegisterScenarios() function

---
+++ scenarios/register.go
@@ -0,0 +1,7 @@
+package scenarios
+
+// RegisterScenarios registers all scenarios. It is intended to be called by Go
+// applications using omes as a library to run scenarios.
+func RegisterScenarios() {
+	// Scenarios are registered automatically on import of this package.
+}
