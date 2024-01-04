04ae117dccff77fdde8206c6ecbccc79fd1f72b1
Dan Davison <dan.davison@temporal.io>
2024-01-04 12:30:51 -0500

    Revert addition of RegisterScenarios function

--- scenarios/register.go
+++
@@ -1,7 +0,0 @@
-package scenarios
-
-// RegisterScenarios registers all scenarios. It is intended to be called by Go
-// applications using omes as a library to run scenarios.
-func RegisterScenarios() {
-	// Scenarios are registered automatically on import of this package.
-}
