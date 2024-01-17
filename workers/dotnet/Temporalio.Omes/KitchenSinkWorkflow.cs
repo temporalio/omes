358fbea2b4be69394c61a82b50640b602fb2ab8c
Spencer Judge <spencer@temporal.io>
2024-01-17 15:57:17 -0800

    Everything implemented. Need to debug some hangs etc.

--- workers/dotnet/Temporalio.Omes/KitchenSinkWorkflow.cs
+++ workers/dotnet/Temporalio.Omes/KitchenSinkWorkflow.cs
@@ -1,16 +1,62 @@
+using System.Threading.Channels;
 using Temporal.Omes.KitchenSink;
 using Temporalio.Activities;
 using Temporalio.Api.Common.V1;
+using Temporalio.Common;
+using Temporalio.Converters;
 using Temporalio.Exceptions;
 using Temporalio.Workflows;
+using RetryPolicy = Temporalio.Api.Common.V1.RetryPolicy;
 
 namespace Temporalio.Omes;
 
 [Workflow("kitchenSink")]
 public class KitchenSinkWorkflow
 {
+    private WorkflowState _workflowState = new();
+
+    private Channel<ActionSet> _actionSetQueue =
+        System.Threading.Channels.Channel.CreateUnbounded<ActionSet>();
+
+    [WorkflowSignal("do_actions_signal")]
+    public async Task DoActionsSignal(DoSignal.Types.DoSignalActions doSignals)
+    {
+        if (doSignals.DoActionsInMain is { } inMain)
+        {
+            _actionSetQueue.Writer.TryWrite(inMain);
+        }
+        else
+        {
+            await HandleActionSet(doSignals.DoActions);
+        }
+    }
+
+    [WorkflowUpdate("do_actions_update")]
+    public async Task<object> DoActionsUpdate(DoActionsUpdate actionsUpdate)
+    {
+        var retval = await HandleActionSet(actionsUpdate.DoActions);
+        if (retval != null)
+        {
+            return retval;
+        }
+
+        return _workflowState;
+    }
+
+    [WorkflowUpdateValidator(nameof(DoActionsUpdate))]
+    public void DoActionsUpdateValidator(DoActionsUpdate actionsUpdate)
+    {
+        if (actionsUpdate.RejectMe != null)
+        {
+            throw new ApplicationFailureException("Rejected");
+        }
+    }
+
+    [WorkflowQuery("report_state")]
+    public WorkflowState ReportState(object _) => _workflowState;
+
     [WorkflowRun]
-    public async Task RunAsync(WorkflowInput? workflowInput)
+    public async Task<Payload?> RunAsync(WorkflowInput? workflowInput)
     {
         // Run all initial input actions
         if (workflowInput?.InitialActions is { } actions)
@@ -20,8 +66,19 @@ public class KitchenSinkWorkflow
                 var returnMe = await HandleActionSet(actionSet);
                 if (returnMe != null)
                 {
-                    return;
+                    return null;
                 }
+            }
+        }
+
+        // Run all actions from signals
+        while (true)
+        {
+            var actionSet = await _actionSetQueue.Reader.ReadAsync();
+            var returnMe = await HandleActionSet(actionSet);
+            if (returnMe != null)
+            {
+                return returnMe;
             }
         }
     }
@@ -72,22 +129,177 @@ public class KitchenSinkWorkflow
         }
         else if (action.ContinueAsNew is { } can)
         {
-            throw Workflow.CreateContinueAsNewException("kitchenSink", can.Arguments);
+            var args = can.Arguments.Select(p => new RawValue(p)).ToArray();
+            throw Workflow.CreateContinueAsNewException("kitchenSink", args);
         }
         else if (action.Timer is { } timer)
         {
-            await Workflow.DelayAsync((int)timer.Milliseconds);
+            var tokenSrc = new CancellationTokenSource();
+            await HandleAwaitableChoice(
+                Workflow.DelayAsync((int)timer.Milliseconds).ContinueWith(_ => true),
+                tokenSrc,
+                timer.AwaitableChoice);
         }
         else if (action.ExecActivity is { } execActivity)
         {
-            // TODO: Cancel handler
-            await LaunchActivity(execActivity);
+            var tokenSrc = new CancellationTokenSource();
+            await HandleAwaitableChoice(
+                LaunchActivity(execActivity, tokenSrc).ContinueWith(_ => true),
+                tokenSrc,
+                execActivity.AwaitableChoice);
         }
+        else if (action.ExecChildWorkflow is { } execChild)
+        {
+            var tokenSrc = new CancellationTokenSource();
+            var childType = execChild.WorkflowType ?? "kitchenSink";
+            var args = execChild.Input.Select(p => new RawValue(p)).ToArray();
+            var options = new ChildWorkflowOptions
+            {
+                Id = execChild.WorkflowId == "" ? null : execChild.WorkflowId,
+                TaskQueue = execChild.TaskQueue,
+                ExecutionTimeout = execChild.WorkflowExecutionTimeout?.ToTimeSpan(),
+                TaskTimeout = execChild.WorkflowTaskTimeout?.ToTimeSpan(),
+                RunTimeout = execChild.WorkflowRunTimeout?.ToTimeSpan()
+            };
+            var childTask = Workflow.StartChildWorkflowAsync(childType, args, options);
+            await HandleAwaitableChoice(childTask, tokenSrc, execChild.AwaitableChoice,
+                afterStartedFn: t => t, afterCompletedFn: async t =>
+                {
+                    var childHandle = await t;
+                    await childHandle.GetResultAsync();
+                });
+        }
+        else if (action.SetPatchMarker is { } setPatchMarker)
+        {
+            var wasPatched = false;
+            if (setPatchMarker.Deprecated)
+            {
+                Workflow.DeprecatePatch(setPatchMarker.PatchId);
+                wasPatched = true;
+            }
+            else
+            {
+                wasPatched = Workflow.Patched(setPatchMarker.PatchId);
+            }
+
+            if (wasPatched)
+            {
+                return await HandleAction(setPatchMarker.InnerAction);
+            }
+        }
+        else if (action.SetWorkflowState is { } setWorkflowState)
+        {
+            _workflowState = setWorkflowState;
+        }
+        else if (action.AwaitWorkflowState is { } awaitWorkflowState)
+        {
+            await Workflow.WaitConditionAsync(() =>
+            {
+                if (!_workflowState.Kvs.TryGetValue(awaitWorkflowState.Key, out string? value))
+                {
+                    return false;
+                }
+
+                return value == awaitWorkflowState.Value;
+            });
+        }
+        else if (action.UpsertMemo is { } upsertMemo)
+        {
+            var memoUpdates = new List<MemoUpdate>();
+            foreach (var keyval in upsertMemo.UpsertedMemo.Fields)
+            {
+                memoUpdates.Add(MemoUpdate.ValueSet(keyval.Key, keyval.Value));
+            }
+
+            Workflow.UpsertMemo(memoUpdates.ToArray());
+        }
+        else if (action.UpsertSearchAttributes is { } upsertSearchAttributes)
+        {
+            var saUpdates = new List<SearchAttributeUpdate>();
+            foreach (var keyval in upsertSearchAttributes.SearchAttributes)
+            {
+                if (keyval.Key.Contains("Keyword"))
+                {
+                    saUpdates.Add(SearchAttributeUpdate.ValueSet(
+                        SearchAttributeKey.CreateKeyword(keyval.Key),
+                        keyval.Value.Data[0].ToString()));
+                }
+                else
+                {
+                    saUpdates.Add(SearchAttributeUpdate.ValueSet(
+                        SearchAttributeKey.CreateDouble(keyval.Key), keyval.Value.Data[0]));
+                }
+            }
+
+            Workflow.UpsertTypedSearchAttributes(saUpdates.ToArray());
+        }
+        else if (action.NestedActionSet is { } nestedActionSet)
+        {
+            return await HandleActionSet(nestedActionSet);
+        }
+        else
+        {
+            throw new ApplicationFailureException("Unrecognized action");
+        }
+
 
         return null;
     }
 
-    private Task LaunchActivity(ExecuteActivityAction eaa)
+    private async Task HandleAwaitableChoice<T>(
+        Task<T> awaitableTask,
+        CancellationTokenSource canceller,
+        AwaitableChoice? choice,
+        Func<Task<T>, Task>? afterStartedFn = null,
+        Func<Task<T>, Task>? afterCompletedFn = null
+    )
+    {
+        afterStartedFn ??= _ => Workflow.DelayAsync(1);
+        afterCompletedFn ??= t => t;
+        choice ??= new AwaitableChoice { WaitFinish = new() };
+
+        var didCancel = false;
+        try
+        {
+            if (choice.Abandon != null)
+            {
+                // Do nothing
+            }
+            else if (choice.CancelBeforeStarted != null)
+            {
+                canceller.Cancel();
+                didCancel = true;
+                await awaitableTask;
+            }
+            else if (choice.CancelAfterStarted != null)
+            {
+                await afterStartedFn(awaitableTask);
+                canceller.Cancel();
+                didCancel = true;
+                await awaitableTask;
+            }
+            else if (choice.CancelAfterCompleted != null)
+            {
+                await afterCompletedFn(awaitableTask);
+                canceller.Cancel();
+                didCancel = true;
+                await awaitableTask;
+            }
+            else
+            {
+                await awaitableTask;
+            }
+        }
+        catch (CanceledFailureException)
+        {
+            if (!didCancel)
+            {
+                throw;
+            }
+        }
+    }
+
+    private Task LaunchActivity(ExecuteActivityAction eaa, CancellationTokenSource tokenSrc)
     {
         var actType = "noop";
         var args = new List<object>();
@@ -104,6 +316,7 @@ public class KitchenSinkWorkflow
                 ScheduleToCloseTimeout = eaa.ScheduleToCloseTimeout?.ToTimeSpan(),
                 ScheduleToStartTimeout = eaa.ScheduleToStartTimeout?.ToTimeSpan(),
                 StartToCloseTimeout = eaa.StartToCloseTimeout?.ToTimeSpan(),
+                CancellationToken = tokenSrc.Token,
                 RetryPolicy =
                     eaa.RetryPolicy != null ? RetryPolicyFromProto(eaa.RetryPolicy) : null
             };
@@ -117,6 +330,7 @@ public class KitchenSinkWorkflow
                 ScheduleToCloseTimeout = eaa.ScheduleToCloseTimeout?.ToTimeSpan(),
                 ScheduleToStartTimeout = eaa.ScheduleToStartTimeout?.ToTimeSpan(),
                 StartToCloseTimeout = eaa.StartToCloseTimeout?.ToTimeSpan(),
+                CancellationToken = tokenSrc.Token,
                 RetryPolicy =
                     eaa.RetryPolicy != null ? RetryPolicyFromProto(eaa.RetryPolicy) : null
             };
