ed1360c67a1bfb049b81458cfcec05d8453a1123
Dan Davison <dan.davison@temporal.io>
2024-07-26 12:04:38 -0400

    Revert "Capture stderr and improve error handling"
    This reverts commit 8bce5c6fa20eb955c6bd167eb3055e5d8a9e27ed.

--- loadgen/fuzz_executor.go
+++ loadgen/fuzz_executor.go
@@ -38,13 +38,10 @@ func (k FuzzExecutor) Run(ctx context.Context, info ScenarioInfo) error {
 	fileOrArgs := k.InitInputs(ctx, info)
 	if fileOrArgs.FilePath != "" {
 		fDat, err := os.ReadFile(fileOrArgs.FilePath)
-		if err != nil {
-			return fmt.Errorf("failed to read file: %w", err)
-		}
 		asTInput := &kitchensink.TestInput{}
 		err = proto.Unmarshal(fDat, asTInput)
 		if err != nil {
-			return fmt.Errorf("failed to unmarshal test input from file: %w", err)
+			return fmt.Errorf("failed to unmarshal test input from file on disk: %w", err)
 		}
 		testInputs = append(testInputs, asTInput)
 	} else if fileOrArgs.Args != nil {
@@ -60,24 +57,14 @@ func (k FuzzExecutor) Run(ctx context.Context, info ScenarioInfo) error {
 			args = append(args, fileOrArgs.Args...)
 			cmd = exec.CommandContext(ctx, "cargo", args...)
 		}
-		// We are capturing stderr for use in the error message. Therefore kitchen-sink-gen should
-		// do neither of the following things:
-		// 1. write to stderr but exit with code 0
-		// 2. write a lot to stderr
+		cmd.Stderr = os.Stderr
 		protoBytes, err := cmd.Output()
 		if err != nil {
-			msg := fmt.Sprintf("failed to run kitchen-sink-gen (%v)", cmd)
-			if exitErr, ok := err.(*exec.ExitError); ok {
-				msg = fmt.Sprintf("%s: %s", msg, exitErr.Stderr)
-			}
-			return fmt.Errorf("%s: %w", msg, err)
+			return fmt.Errorf("failed to run rust generator (%v): %w", cmd, err)
 		}
-		testInput := &kitchensink.TestInput{}
-		err = proto.Unmarshal(protoBytes, testInput)
-		if err != nil {
-			return fmt.Errorf("failed to unmarshal kitchen-sink-gen output: %w", err)
-		}
-		testInputs = append(testInputs, testInput)
+		asTInput := &kitchensink.TestInput{}
+		err = proto.Unmarshal(protoBytes, asTInput)
+		testInputs = append(testInputs, asTInput)
 	} else {
 		return fmt.Errorf("InitInputs must specify either a file or args")
 	}
