8586f26d0d2b4b0bd8c4e676608f7379fa12b623
Spencer Judge <spencer@temporal.io>
2024-08-06 13:23:35 -0700

    Switch to updated container / net8

--- cmd/prepare_worker.go
+++ cmd/prepare_worker.go
@@ -229,7 +229,7 @@ func (b *workerBuilder) buildDotNet(ctx context.Context, baseDir string) (sdkbui
 		CsprojContents: `<Project Sdk="Microsoft.NET.Sdk">
 			<PropertyGroup>
 				<OutputType>Exe</OutputType>
-				<TargetFramework>net7.0</TargetFramework>
+				<TargetFramework>net8.0</TargetFramework>
 			</PropertyGroup>
 			<ItemGroup>
 				<ProjectReference Include="../Temporalio.Omes.csproj" />
--- dockerfiles/dotnet.Dockerfile
+++ dockerfiles/dotnet.Dockerfile
@@ -1,5 +1,5 @@
 # Build in a full featured container
-FROM mcr.microsoft.com/dotnet/sdk:7.0-jammy as build
+FROM mcr.microsoft.com/dotnet/sdk:8.0-jammy as build
 
 # Install protobuf compiler and build tools
 RUN apt-get update \
