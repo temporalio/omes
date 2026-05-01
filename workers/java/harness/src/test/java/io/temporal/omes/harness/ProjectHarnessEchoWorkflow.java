8cbe82defa50bcc1916a588a75684d5646e74e17
Thomas Hardy <thomas.hardy@temporal.io>
2026-05-01 12:03:16 -0400

    harness implementation

--- workers/java/harness/src/test/java/io/temporal/omes/harness/ProjectHarnessEchoWorkflow.java
+++ workers/java/harness/src/test/java/io/temporal/omes/harness/ProjectHarnessEchoWorkflow.java
@@ -4,7 +4,16 @@ import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 
 @WorkflowInterface
-public interface ProjectHarnessEchoWorkflow {
-  @WorkflowMethod
-  String run(String payload);
+  public interface ProjectHarnessEchoWorkflow {
+    @WorkflowMethod
+    String run(String payload);
+  }
+
+class ProjectHarnessEchoWorkflowImpl implements ProjectHarnessEchoWorkflow {
+  public ProjectHarnessEchoWorkflowImpl() {}
+
+  @Override
+  public String run(String payload) {
+    return payload;
+  }
 }
