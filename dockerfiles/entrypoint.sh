9aa6efccb8fe9adf2f4b6f4de77c8aab034a7493
Thomas Hardy <thomas.hardy@temporal.io>
2026-05-29 16:20:04 -0400

    add a comment explaining the entrypoint existence

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
 
+# This image normally behaves like the other worker images: callers pass worker
+# flags and we add run-worker plus the prepared Python package defaults. Explicit
+# omes subcommands pass through so the same image can run project scenarios.
 case "${1:-}" in
   run-scenario|cleanup-scenario|list-scenarios|prepare-worker|run-scenario-with-worker|completion|help)
     exec /app/temporal-omes "$@"
