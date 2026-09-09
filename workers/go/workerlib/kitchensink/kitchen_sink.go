eea28759fa37b8342eca8b961d4cb6bf124f2b4f
Stephan Behnke <stephanos@users.noreply.github.com>
2026-09-08 19:53:42 -0700

    Update kitchen_sink.go

--- workers/go/workerlib/kitchensink/kitchen_sink.go
+++ workers/go/workerlib/kitchensink/kitchen_sink.go
@@ -486,8 +486,12 @@ func handleNexusOperation(ctx workflow.Context, nexusOp *kitchensink.ExecuteNexu
 }
 
 func handleSendSignal(ctx workflow.Context, ws *KSWorkflowState, action *kitchensink.SendSignalAction) error {
+	var arg *common.Payload
+	if len(action.Args) > 0 {
+		arg = action.Args[0]
+	}
 	return withAwaitableChoiceCustom(ctx, ws, func(ctx workflow.Context) workflow.Future {
-		return workflow.SignalExternalWorkflow(ctx, action.WorkflowId, action.RunId, action.SignalName, nil)
+		return workflow.SignalExternalWorkflow(ctx, action.WorkflowId, action.RunId, action.SignalName, arg)
 	}, action.AwaitableChoice,
 		func(ctx workflow.Context, fut workflow.Future) error {
 			return fut.Get(ctx, nil)
