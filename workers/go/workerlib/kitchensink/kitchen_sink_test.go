60ef459cd7433fb983f337de0e48339835ac9a19
Stephan Behnke <stephanos@users.noreply.github.com>
2026-09-10 08:38:33 -0700

    Delete kitchen_sink_test.go

--- workers/go/workerlib/kitchensink/kitchen_sink_test.go
+++
@@ -1,9 +0,0 @@
-package kitchensink
-
-import (
-	"github.com/nexus-rpc/sdk-go/nexus"
-	ks "github.com/temporalio/omes/loadgen/kitchensink"
-	"go.temporal.io/sdk/converter"
-)
-
-var _ nexus.Operation[*ks.NexusOperationRequest, converter.RawValue] = KitchenSinkNexusOperation
